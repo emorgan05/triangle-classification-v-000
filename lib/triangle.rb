ebb3f1dc2d8bea58b1da39755c9ffee260091333
Emily Morgan <emorgan05@gmail.com>
2017-10-03 18:29:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
     elsif @length_1 == @length_2 || @length_1 == @length_3
       :isosceles
     elsif @length_1 != @length_2 && @length_1 != @length_3 && @length_2 != @length_3
-      :scaline
+      :scalene
     end
   end
 end
