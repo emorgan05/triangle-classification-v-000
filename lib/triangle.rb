33b5c12ac49d5130eeba860b732f00da66ef2eed
Emily Morgan <emorgan05@gmail.com>
2017-10-03 18:54:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
 
   def kind
     if @length_1 == 0 && @length_2 == 0 && @length_3 == 0
-        raise TriangleError
+        # raise TriangleError
     elsif @length_1 + @length_2 <= @length_3 || @length_1 + @length_3 <= @length_2 || @length_2 + @length_3 <= @length_1
-        raise TriangleError
+        # raise TriangleError
     elsif @length_1 == @length_2 && @length_1 == @length_3
       :equilateral
     elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
