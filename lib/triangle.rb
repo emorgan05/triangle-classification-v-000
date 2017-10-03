fe712daa73cb50a87d9c7d2110ddf2458b9a6261
Emily Morgan <emorgan05@gmail.com>
2017-10-03 18:38:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,8 +20,7 @@ class Triangle
       rescue TriangleError => error
         puts error.message
       end
-    end
-    if @length_1 == @length_2 && @length_1 == @length_3
+    elsif @length_1 == @length_2 && @length_1 == @length_3
       :equilateral
     elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
       :isosceles
