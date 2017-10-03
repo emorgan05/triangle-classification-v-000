cad4d743deb96d72f2321d3d4b9d7533da3d617b
Emily Morgan <emorgan05@gmail.com>
2017-10-03 19:11:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     if @length_1 == 0 && @length_2 == 0 && @length_3 == 0
         raise TriangleError
     elsif @length_1 + @length_2 <= @length_3 || @length_1 + @length_3 <= @length_2 || @length_2 + @length_3 <= @length_1
-        # raise TriangleError
+        raise TriangleError
     elsif @length_1 == @length_2 && @length_1 == @length_3
       :equilateral
     elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
